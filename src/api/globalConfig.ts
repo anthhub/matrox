33db9274a5eacdbe327e7a00c9c0cf03a9a7fbac
qiyuan.tang01 <qiyuan.tang01@liulishuo.com>
2020-05-14 00:35:47 +0800

    tmp

--- src/api/globalConfig.ts
+++ src/api/globalConfig.ts
@@ -2,6 +2,19 @@ import { GlobalOptions } from '../types/store'
 
 export let globalOptions: GlobalOptions
 
+/**
+ * config add apply options for all store
+ *
+ * @example
+ * ```ts
+ * import { GlobalOptions } from 'matrox'
+ * globalConfig({})
+ * ```
+ *
+ * @param  {GlobalOptions} options global options
+ *
+ * @see https://github.com/anthhub/matrox#globalConfig
+ */
 const globalConfig = (options: GlobalOptions) => {
   globalOptions = options
 }
