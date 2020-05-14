ddcdce595023374623477d11f740cf7ea5818d14
qiyuan.tang01 <qiyuan.tang01@liulishuo.com>
2020-05-14 14:04:32 +0800

    fix: remove preload

--- src/api/createStore.ts
+++ src/api/createStore.ts
@@ -2,7 +2,6 @@ import getInjection from './getInjection'
 import StoreBase from './StoreBase'
 import useInjection from './useInjection'
 import injection from './injection'
-import preload from './preload'
 import { Payload } from '../types/StoreBase'
 import { Constructor } from '../types/store'
 
@@ -13,7 +12,9 @@ const createStore = <T extends StoreBase<T>, U extends Payload<T>>(
   const injectStore = (args?: U) => injection(InjectedStoreClass, args)
   const getStore = (args?: U) => getInjection(InjectedStoreClass, args)
 
-  const preloadStore = (args?: U) => preload(InjectedStoreClass, args)
+  const preloadStore = (args?: U) => {
+    getStore(args)
+  }
 
   const getState = () => {
     return JSON.parse(JSON.stringify(getStore() || {}))
